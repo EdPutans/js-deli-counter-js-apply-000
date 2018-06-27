4559a144f36fee688bcfc6bf0c27ba77f92b1a34
Ed Putans <edputans@gmail.com>
2018-06-27 15:08:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 //The function, takeANumber, should accept the current line of people, katzDeliLine, along with the new person's name as parameters. The function should return their position in line.//
 
-takeANumber = function(katzDeliLine, name){
+takeANumber = function(katzDeliLine, name) {
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`}
 /*.length works since the last person in array is the new one*/}
 
 
