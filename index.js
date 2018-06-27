761944b151facdf150794650375a097dc60c5426
Ed Putans <edputans@gmail.com>
2018-06-27 14:37:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 
-var i=0
+//The function, takeANumber, should accept the current line of people, katzDeliLine, along with the new person's name as parameters. The function should return their position in line.//
+
 takeANumber = function(katzDeliLine, name){
   katzDeliLine.push(name)
-  var a=`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
-  i++
-  return a
-  
-}
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+/*.length works since the last person in array is the new one*/}
+
+
+
+//This function should return the first person in line and then remove that individual from the line. If there is nobody in line, it should return "There is nobody waiting to be served!"
 
   function nowServing(deliLine){
-    while(deliLine.length>0){
-      var a=deliLine.shift()
-      return `Currently serving ${a}.`
+    if (deliLine.length>0){
+      var line=deliLine.shift()
+      return (`Currently serving ${line}.`)
     }
     if(deliLine.length===0){
-      return("There is nobody waiting to be served!")
-    }
+      return("There is nobody waiting to be served!")}
   }
   
   
-  
-  
-  
+  //Build a function currentLine that returns the current line. For example, if katzDeliLine is currently ["Ada", "Grace"], currentLine(katzDeliLine) would return "The line is currently: 1. Ada, 2. Grace". If there is nobody in line, it should return "The line is currently empty."
   function currentLine(line){
-   if (line.length>0){
-   var linePeeps=[]
-   for (let i=0; i<line.length; i++)      
-    linePeeps.push(` ${i+1}. ${line[i]}`)
-   return (`The line is currently:` + linePeeps.join())}
-   if (line.length===0){
-     return('The line is currently empty.')
-   }
+    if (line.length>0){
+      var linePeeps=[]
+      for (let i=0; i<line.length; i++)      
+       linePeeps.push(` ${i+1}. ${line[i]}`)
+       //i+1 - customer position, line - name
+        return (`The line is currently:` + linePeeps.join(','))}
+        
+    if (line.length===0){
+       return('The line is currently empty.')
     }
+}
   
   
   
